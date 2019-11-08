48d1774d124c954a55788dfa448ab93e1c383fad
Jess <jlswans1@gmail.com>
2019-11-07 23:08:19 -0700

    Create the form

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Wraps by Jess</h1>
+
+        <p>What is your full name?</p>
+        <input type="text" name="name" placeholder="Full Name"/>
+
+        <p>What's your email address?</p>
+        <input type="email" name="email" placeholder="Email Address"/>
+
+        <p>What's the best phone number to reach you at?</p>
+        <input type="text" name="phone" placeholder="Phone Number"/>
+
+        <input-multiple name="gifts" placeholder="Description" label="Describe each of the gifts you want wrapped."/>
+        <button v-on:click="submitForm($event)">Submit</button>
+    </div>
+</template>
+
+<script>
+import InputMultiple from './forms/InputMultiple.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        InputMultiple
+    },
+    methods: {
+        submitForm: function(event) {
+            event.preventDefault();
+            console.log('submitted');
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        font-weight: 400;
+    }
+</style>
+
+
---
+++ src/components/forms/InputMultiple.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <p>{{label}}</p>
+        <div class="form-group"
+            v-for="item in items"
+            :key="item.key">
+            <input type="text"
+            :placeholder="placeholder"
+            v-model="item.description"
+            :tabindex="setTabIndex(item)">
+            <button v-on:click="addItem()">+</button>
+            <button v-on:click="removeItem(item)">-</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputMultiple',
+    props: [
+        'placeholder',
+        'label',
+        'name'
+    ],
+    data: function() {
+        return {
+            items: [
+                {
+                    key: this.generateKey()
+                }
+            ]
+        }
+    },
+    methods: {
+        addItem: function() {
+            let newKey = this.generateKey();
+            this.items.push({key: newKey, description: this.description});
+        },
+        removeItem: function(item) {
+            const index = this.items.map(function(object) {return object.key}).indexOf(item.key);
+            this.items.splice(index, 1);
+        },
+        setTabIndex: function(item) {
+            let index = this.items.map(function(object) {return object.key}).indexOf(item.key);
+            return index += 1;
+        },
+        generateKey: function() {
+            return Math.floor(Math.random() * Math.floor(10000000));
+        },
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
